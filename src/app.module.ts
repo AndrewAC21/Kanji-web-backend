2d468c4e434eaa9b62a927fd16c9ab463e9466dc
AndresA <oskr.ac73@gmail.com>
2022-08-06 21:34:25 -0500

    create env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { KanjisModule } from './kanjis/kanjis.module';
+import config from 'config';
 
 @Module({
-  imports: [AuthModule, UsersModule, DatabaseModule, KanjisModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    DatabaseModule,
+    KanjisModule,
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
